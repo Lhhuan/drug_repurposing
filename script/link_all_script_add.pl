947d34a6bae824ca1706d9e562fbde554b5922e8
hstyle <lhhuan01@163.com>
2018-11-27 14:19:00 +0800

    add_s

--- script/link_all_script_add.pl
+++ script/link_all_script_add.pl
@@ -6,7 +6,7 @@ use File::Basename;
 
 my $dir_out   = "/f/mulinlab/huan/Script_backup/Huan_link_all_script/";
 mkdir $dir_out unless -d $dir_out;
-
+#
 chdir "/f/mulinlab/huan/";
 system "find -name *.pl > /f/mulinlab/huan/perl_script";
 system "find -name *.R > /f/mulinlab/huan/R_script";
@@ -20,19 +20,26 @@ while(<$I1>)
 {
     chomp;
     my $script = $_;
-    my $file = basename($script);
-    my $dir = dirname($script);
-    my $do = "$dir_out/$dir";
-    # mkdir  $do unless -d $do;
-    unless(-e $do ){
-        system "mkdir -p $do";
+    unless($script=~/^\.\/anaconda/){
+        unless($script=~/^\.\/Script_backup/){
+            unless($script=~/^\.\/php/){
+                my $file = basename($script);
+                my $dir = dirname($script);
+                my $do = "$dir_out/$dir";
+                # mkdir  $do unless -d $do;
+                unless(-e $do ){
+                    system "mkdir -p $do";
+                }
+                my $new_file =" $do/$file";
+                unless(-e $new_file){
+                    my $link1 = "ln $script $do/" ;
+                    system "$link1\n";
+                    # print "$link1\n";
+                }
+            }
+        }
     }
-    my $new_file =" $do/$file";
-    unless(-e $new_file){
-        my $link1 = "ln $script $do/" ;
-        system "$link1\n";
-        # print "$link1\n";
-    }
+    
 }
 
 my $f2 ="/f/mulinlab/huan/R_script";
@@ -41,19 +48,26 @@ while(<$I2>)
 {
     chomp;
     my $script = $_;
-    my $file = basename($script);
-    my $dir = dirname($script);
-    my $do = "$dir_out/$dir";
-    # mkdir  $do unless -d $do;
-    unless(-e $do ){
-        system "mkdir -p $do";
+    unless($script=~/^\.\/anaconda/){
+        unless($script=~/^\.\/Script_backup/){
+            unless($script=~/^\.\/php/){
+                my $file = basename($script);
+                my $dir = dirname($script);
+                my $do = "$dir_out/$dir";
+                # mkdir  $do unless -d $do;
+                unless(-e $do ){
+                    system "mkdir -p $do";
+                }
+                my $new_file =" $do/$file";
+                unless(-e $new_file){
+                    my $link1 = "ln $script $do/" ;
+                    system "$link1\n";
+                    # print "$link1\n";
+                }
+            }
+        }
     }
-    my $new_file =" $do/$file";
-    unless(-e $new_file){
-        my $link1 = "ln $script $do/" ;
-        system "$link1\n";
-        # print "$link1\n";
-    }
+    
 }
 
 my $f3 ="/f/mulinlab/huan/all_sh_script";
@@ -62,19 +76,26 @@ while(<$I3>)
 {
     chomp;
     my $script = $_;
-    my $file = basename($script);
-    my $dir = dirname($script);
-    my $do = "$dir_out/$dir";
-    # mkdir  $do unless -d $do;
-    unless(-e $do ){
-        system "mkdir -p $do";
+    unless($script=~/^\.\/anaconda/){
+        unless($script=~/^\.\/Script_backup/){
+            unless($script=~/^\.\/php/){
+                my $file = basename($script);
+                my $dir = dirname($script);
+                my $do = "$dir_out/$dir";
+                # mkdir  $do unless -d $do;
+                unless(-e $do ){
+                    system "mkdir -p $do";
+                }
+                my $new_file =" $do/$file";
+                unless(-e $new_file){
+                    my $link1 = "ln $script $do/" ;
+                    system "$link1\n";
+                    # print "$link1\n";
+                }
+            }
+        }
     }
-    my $new_file =" $do/$file";
-    unless(-e $new_file){
-        my $link1 = "ln $script $do/" ;
-        system "$link1\n";
-        # print "$link1\n";
-    }
+    
 }
 
 my $f4 ="/f/mulinlab/huan/readme";
@@ -83,19 +104,26 @@ while(<$I4>)
 {
     chomp;
     my $script = $_;
-    my $file = basename($script);
-    my $dir = dirname($script);
-    my $do = "$dir_out/$dir";
-    # mkdir  $do unless -d $do;
-    unless(-e $do ){
-        system "mkdir -p $do";
+    unless($script=~/^\.\/anaconda/){
+        unless($script=~/^\.\/Script_backup/){
+            unless($script=~/^\.\/php/){
+                my $file = basename($script);
+                my $dir = dirname($script);
+                my $do = "$dir_out/$dir";
+                # mkdir  $do unless -d $do;
+                unless(-e $do ){
+                    system "mkdir -p $do";
+                }
+                my $new_file =" $do/$file";
+                unless(-e $new_file){
+                    my $link1 = "ln $script $do/" ;
+                    system "$link1\n";
+                    # print "$link1\n";
+                }
+            }
+        }
     }
-    my $new_file =" $do/$file";
-    unless(-e $new_file){
-        my $link1 = "ln $script $do/" ;
-        system "$link1\n";
-        # print "$link1\n";
-    }
+    
 }
 
 my $f5 ="/f/mulinlab/huan/python_script";
@@ -104,22 +132,29 @@ while(<$I5>)
 {
     chomp;
     my $script = $_;
-    my $file = basename($script);
-    my $dir = dirname($script);
-    my $do = "$dir_out/$dir";
-    # mkdir  $do unless -d $do;
-    unless(-e $do ){
-        system "mkdir -p $do";
+    unless($script=~/^\.\/anaconda/){
+        unless($script=~/^\.\/Script_backup/){
+            unless($script=~/^\.\/php/){
+                my $file = basename($script);
+                my $dir = dirname($script);
+                my $do = "$dir_out/$dir";
+                # mkdir  $do unless -d $do;
+                unless(-e $do ){
+                    system "mkdir -p $do";
+                }
+                my $new_file =" $do/$file";
+                unless(-e $new_file){
+                    my $link1 = "ln $script $do/" ;
+                    system "$link1\n";
+                    # print "$link1\n";
+                }
+            }
+        }
     }
-    my $new_file =" $do/$file";
-    unless(-e $new_file){
-        my $link1 = "ln $script $do/" ;
-        system "$link1\n";
-        # print "$link1\n";
-    }
+    
 }
 
-system "rm -r /f/mulinlab/huan/Script_backup/Huan_link_all_script/anaconda3/";
-system "rm -r /f/mulinlab/huan/Script_backup/Huan_link_all_script/anaconda/";
-system "rm -r /f/mulinlab/huan/Script_backup/Huan_link_all_script/anaconda2/";
-system "rm -r /f/mulinlab/huan/Script_backup/Huan_link_all_script/oncotree/";
+# system "rm -r /f/mulinlab/huan/Script_backup/Huan_link_all_script/anaconda3/";
+# system "rm -r /f/mulinlab/huan/Script_backup/Huan_link_all_script/anaconda/";
+# system "rm -r /f/mulinlab/huan/Script_backup/Huan_link_all_script/anaconda2/";
+# system "rm -r /f/mulinlab/huan/Script_backup/Huan_link_all_script/oncotree/";
